def16ab9fe7ebb67a367ca094eb643aac744e7b0
Nwala Enyinna <alexiusnwala@gmail.com>
2022-05-19 00:04:54 +0100

    not found styled a bit

--- App/src/Components/NotFound.js
+++ App/src/Components/NotFound.js
@@ -1,9 +1,19 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+
+import '../styles/notFound.css'
 
 function NotFound() {
-    let location = useLocation();
-  return <h1>NotFound {location.pathname}</h1>;
+  let location = useLocation();
+  return (
+    <div className="notFound">
+      <h1>404</h1>
+      <p>{location.pathname} not found.</p>
+      <div>
+          <Link to={'/'} >Home?</Link>
+      </div>
+    </div>
+  );
 }
 
 export default NotFound;
