42d4724fcea3668e7b2fe7e25fd9d55450eb205b
Nwala Enyinna <alexiusnwala@gmail.com>
2022-06-26 09:09:42 +0100

    Changed site name

--- src/api/config.js
+++ src/api/config.js
@@ -1,3 +1,3 @@
 export const
-REACT_APP_BASE_URL = "https://courier-finder.netlify.app",
+REACT_APP_BASE_URL = "https://emissar.netlify.app",
 REACT_APP_API_URL = "https://emissar-server.herokuapp.com"
